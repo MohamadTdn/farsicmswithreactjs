e543756beab49d123e6e381f94cc6d419fa51877
MohamadTdn <mohamadmt1579@gmail.com>
2024-08-11 15:24:26 +0330

    add chart component

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import './Chart.css'
+import React from "react";
+import "./Chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import {xAxis} from '../../Datas'
 
 function Chart() {
   return (
-    <div>Chart</div>
-  )
+    <div className="Chart">
+      <ResponsiveContainer aspect={3} width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={xAxis}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <XAxis dataKey='name' />
+          <YAxis dataKey='sale' />
+          <Tooltip />
+          <CartesianGrid stroke="#f2f2f2" strokeDasharray={10} />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey='sale'
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
 
-export default Chart
+export default Chart;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import Statistics from '../Components/Statostocs/Statistics'
-import {staticDatas} from '../Datas'
+import React from "react";
+import Statistics from "../Components/Statostocs/Statistics";
+import { staticDatas } from "../Datas";
+import Chart from "../Components/Chart/Chart";
 
 function Home() {
   return (
-    <div className='Home'>
+    <div className="Home">
       <h4>آمار</h4>
-      <div className='Main-content' style={{display: 'flex', justifyContent: 'space-evenly'}}>
-        {staticDatas.map(data => {
-          return (
-            <Statistics {...data} />
-          )
+      <div
+        className="Main-content"
+        style={{ display: "flex", justifyContent: "space-evenly" }}
+      >
+        {staticDatas.map((data) => {
+          return <Statistics {...data} />;
         })}
       </div>
       <h4>نمودار</h4>
       <div>
-        
+        <Chart/>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
