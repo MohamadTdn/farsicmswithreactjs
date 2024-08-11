63194fbb469c8af60769ca102d366bc5391ac7d6
MohamadTdn <mohamadmt1579@gmail.com>
2024-08-11 21:22:58 +0330

    add new products component

--- src/Pages/NewProducts.js
+++ src/Pages/NewProducts.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function NewProducts() {
   return (
-    <div>NewProducts</div>
-  )
+    <div>
+      <h4>محصول جدید</h4>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>نام محصول</Form.Label>
+          <Form.Control type="text" placeholder="نام محصول را وارد کنید" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>تعداد</Form.Label>
+          <Form.Control type="number" placeholder="تعداد را وارد کنید" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>قیمت</Form.Label>
+          <Form.Control type="number" placeholder="قیمت را وارد کنید"/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>آدرس عکس</Form.Label>
+          <Form.Control type="text" placeholder="آدرس عکس را وارد کنید" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
-export default NewProducts
+export default NewProducts;
